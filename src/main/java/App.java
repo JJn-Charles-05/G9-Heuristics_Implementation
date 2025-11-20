7a44f76e92ebd5fc29df048c5f180432c5cf7207
JJn-Charles-05 <jjnchar.business@gmail.com>
2025-11-20 01:18:35 -0500

    JavaFX added to project (woo!); beginning work on the GUI

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,9 +1,24 @@
 import core.Settings;
 import sim.Simulation;
 import ui.ConsoleRenderer;
+import javafx.scene.Scene;
+import javafx.application.Application;
+import javafx.stage.Stage;
+import javafx.scene.control.Label;
 
-public class App {
+public class App extends Application {
+    @Override
+    public void start(Stage stage) {
+        Label label = new Label("Hello, World!");
+        Scene scene = new Scene(label, 400, 400);
+        stage.setTitle("JavaFX");
+        stage.setScene(scene);
+        stage.show();
+    }
+
     public static void main(String[] args) {
+        launch(args);
+
         Settings s = new Settings();
         s.width = 20;
         s.height = 12;
